159da715bc156869d41a564b7ffa4530ae32bf15
Bram Hoendervangers <bram.hoendervangers@gmail.com>
2018-03-15 20:38:06 +0100

    Add initial polybar config WIP

---
+++ config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch top and bottom bar
+polybar top -r &
+polybar bottom -r &
+
+echo "Bars launched..."
